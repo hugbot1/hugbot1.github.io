ff7f353a8cea40c022b6dc8e2160660dc2125413
hugbot1 <132775034+hugbot1@users.noreply.github.com>
2023-11-18 14:09:28 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+function alertButton() {
+    alert("that button was so cool")
+}
+function alertnew() {
+    alert("thanks for pushing this button too")
+}
+function alertnewer() {
+    alert("then press more buttons")
+}
+function alertnewerr() {
+    alert("wow a alert")
+}
+function alertnew1() {
+    alert("more buttons")
+}
+function alertnew2() {
+    alert("WOW SO MUCH BUTTONS!")
+}
+function alertnew3() {
+    alert("wait why are we going up")
+}
+function alertnew4() {
+    alert("huh")
+}
+function alertnew5() {
+    alert("what ever")
+}
+function alertnew6() {
+    alert("how long will this website be up")
+    alert("i dont know")
+    alert("wait...")
+    alert("dual messges")
+    alert("wow!")
+}
+function alertnew7() {
+    alert("wow is this the final button")
+    alert("it might not")
+    alert("or might be")
+    alert("well.")
+    alert("this is the end of your button journey")
+    alert("this was fun")
+    alert("but i think you should explore for some other things on the web")
+    alert("WAIT!!!!")
+    alert("i almost forgot")
+    alert("Made by hugbot1 aka handle")
+    alert("you can now close this tab")
+}
