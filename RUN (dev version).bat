1cb4b736cd14c34d0271d1cff6689cf52d5f8ab0
hugbot1 <132775034+hugbot1@users.noreply.github.com>
2024-02-15 19:37:18 -0600

    Add files via upload

---
+++ RUN (dev version).bat
@@ -0,0 +1,11 @@
+:b
+@echo off
+echo Input key (dev)
+set /p input=
+if /i %input%==b8A4-329Yr34-0324Maosda-fewjlkjkljdejd32482Abbdsawda goto a
+if / i not %input%== b8A4-329Yr34-0324Maosda-fewjlkjkljdejd32482Abbdsawda goto b
+
+:a
+echo Success! server is runing keep this window open do not press any key in this window to keep this running
+pause
+exit
